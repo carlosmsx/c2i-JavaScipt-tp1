ee84895ddb7e45072778de44d40b15cf7e84643a
carlosmsx <carlosmsx@gmail.com>
2022-05-17 01:23:48 -0300

    agregados conceptos de variables

---
+++ js/1-variables.js
@@ -0,0 +1,16 @@
+// escribir sobre el html
+document.write("Hola Mundo");
+
+//mensaje por consola
+console.log("hola mundo");
+
+//alert("Hola comision c2i");
+
+//crear, declarar, inicializar variables
+
+//var: en desuso
+//let
+//const
+
+let persona = 'Nilosaurio';
+let persona2;
