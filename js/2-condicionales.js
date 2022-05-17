fe7b8ab71e8f6f01743ebf66c5dfb483d98d0e96
carlosmsx <carlosmsx@gmail.com>
2022-05-17 10:41:46 -0300

    agregada practica de condicionales

---
+++ js/2-condicionales.js
@@ -0,0 +1,6 @@
+let numero1 = parseInt(prompt('Ingrese el primer numero'));
+let numero2 = parseInt(prompt('Ingrese el segundo numero'));
+
+if (numero1 > numero2) {
+    document.write('El numero mayor es el primero: ' + numero1);
+}
