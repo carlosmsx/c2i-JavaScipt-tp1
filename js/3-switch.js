97b14acb59a92ca1404a18d21afb5780b1a6703a
carlosmsx <carlosmsx@gmail.com>
2022-05-17 10:44:33 -0300

    agregada practica de switch, ejemplo sin switch

---
+++ js/3-switch.js
@@ -0,0 +1,14 @@
+let estacion = prompt('Ingrese su estacion preferida: 1-Verano, 2-Otoño, 3-Invierno, 4-Primavera');
+
+if (estacion==='1') {
+    document.write('Sandwich de miga');
+} else if (estacion==='2') {
+    document.write('Locro');
+} else if (estacion==='3') {
+    document.write('Guiso');
+} else if (estacion==='4') {
+    document.write('Cerveza');
+} else {
+    document.write('Ingreso una opcion erronea');
+}
+
