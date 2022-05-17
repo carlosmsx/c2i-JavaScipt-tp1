bdddfb86b1b76e3cf6fd96bdd0c0f0244cc437e5
carlosmsx <carlosmsx@gmail.com>
2022-05-17 11:03:21 -0300

    cambiado el codigo para usar switch

--- js/3-switch.js
+++ js/3-switch.js
@@ -1,14 +1,31 @@
 let estacion = prompt('Ingrese su estacion preferida: 1-Verano, 2-Otoño, 3-Invierno, 4-Primavera');
 
-if (estacion==='1') {
-    document.write('Sandwich de miga');
-} else if (estacion==='2') {
-    document.write('Locro');
-} else if (estacion==='3') {
-    document.write('Guiso');
-} else if (estacion==='4') {
-    document.write('Cerveza');
-} else {
-    document.write('Ingreso una opcion erronea');
-}
+// if (estacion==='1') {
+//     document.write('Sandwich de miga');
+// } else if (estacion==='2') {
+//     document.write('Locro');
+// } else if (estacion==='3') {
+//     document.write('Guiso');
+// } else if (estacion==='4') {
+//     document.write('Cerveza');
+// } else {
+//     document.write('Ingreso una opcion erronea');
+// }
 
+switch (estacion)
+{
+    case '1':
+        document.write('Sandwich de miga');
+        break;
+    case '2':
+        document.write('Locro');
+        break;
+    case '3':
+        document.write('Guiso');
+        break;
+    case '4':
+        document.write('Cerveza');
+        break;
+    default:
+        document.write('Ingreso una opcion erronea');
+}
