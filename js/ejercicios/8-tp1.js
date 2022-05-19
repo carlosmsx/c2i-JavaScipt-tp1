567408320aa016d63f3b4231c2eb448f51f49d35
carlosmsx <carlosmsx@gmail.com>
2022-05-19 02:27:11 -0300

    agregado ejercicio 8

---
+++ js/ejercicios/8-tp1.js
@@ -0,0 +1,5 @@
+let numero = parseInt(prompt("Ingrese un numero entero"));
+if (numero%2==0)
+    document.write('El numero ingresado es divisible por 2: ' + numero);
+else
+    document.write('El numero ingresado no es divisible por 2: ' + numero);
