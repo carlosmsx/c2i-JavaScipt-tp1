79757025a3e814d318a46a9b4fba6b8d553b7a3a
carlosmsx <carlosmsx@gmail.com>
2022-05-19 02:53:01 -0300

    arreglado un detalle en la lista de numeros presentados en el mensaje de resultados

--- js/ejercicios/10-tp1.js
+++ js/ejercicios/10-tp1.js
@@ -1,5 +1,5 @@
 let numero = parseInt(prompt("Ingrese un numero entero"));
 if (numero%2==0 || numero%3==0 || numero%5==0 || numero%7==0)
-    document.write(numero+' es divisible por alguno de estos numeros: 2,3,5,7 ');
+    document.write(numero+' es divisible por alguno de estos numeros: 2, 3, 5, 7');
 else
-    document.write(numero+' no es divisible por ninguno de estos numeros: 2,3,5,7')
+    document.write(numero+' no es divisible por ninguno de estos numeros: 2, 3, 5, 7')
